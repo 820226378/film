50f460e4c271dc130e4b4b25be56ca9372cd89b7
jyd <jydzsx@163.com>
2017-07-20 15:01:19 +0800

    initial project version

---
+++ modules/user/userLogin.js
@@ -0,0 +1,15 @@
+
+var pool= require('../mysql/dbOperation');
+
+module.exports =function(username,password,callback) {
+    var sql="SELECT * FROM b_user WHERE NAME = '"+ username + "' AND PASSWORD = '" + password+"'";
+    console.log(sql);
+    pool(sql).then(function(data){
+        if(data.length>0)
+            callback({status:1});
+        else
+            callback({status:0})
+    },function(value){
+        callback({status:0,reason:'error'});
+    })
+};
